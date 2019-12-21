ecbb21011ed34d928ccca0163ed7900692c4484f
profmou <52830477@qq.com>
2019-12-21 21:33:48 +0800

    1、配合android客户端的状态，注释掉发送的两句代码

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ print('Waiting for connection...')
 
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
-    sock.send(b'Welcome!')
+    #sock.send(b'Welcome!')
     while True:
         data = sock.recv(1024)
         time.sleep(1)
         if not data or data.decode('utf-8') == 'exit':
             break
-        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        #sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
         print('%s' % data.decode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
